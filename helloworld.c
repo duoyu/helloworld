820da12ebd4f3b98f79787bf82fb574fe417cd98
duoyu <275866494@qq.com>
2016-03-31 14:43:01 +0800

    m3

--- helloworld.c
+++ helloworld.c
@@ -6,6 +6,9 @@ int main(void)
 	printf("Helloworld!\n");
 	printf("I am a changed line!\n");
 	printf("I am a new line?\n");
+	printf("I am a stash line?\n");
+	printf("I am a new line?\n");
+	printf("I am a new line?\n");
 
 	return 0;
 }
