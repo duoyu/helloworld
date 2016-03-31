a8ee4f095069ef3cba0f92d3992366f43b018e20
duoyu <275866494@qq.com>
2016-03-31 14:15:32 +0800

    b1

--- helloworld.c
+++ helloworld.c
@@ -6,7 +6,13 @@ int main(void)
 	printf("Helloworld!\n");
 	printf("I am a changed line!\n");
 	printf("I am a new line?\n");
-	printf("I am a stash line?\n");
+	printf("I am a new line?\n");
+	printf("I am a new line?\n");
+	printf("I am a new line?\n");
+	printf("I am a new line?\n");
+	printf("I am a new line?\n");
+	printf("I am a new line?\n");
+	printf("I am a new line?\n");
 	printf("I am a new line?\n");
 	printf("I am a new line?\n");
 
