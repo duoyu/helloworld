5f1946006d532418955aa2744f1b432247eed92a
duoyu <275866494@qq.com>
2016-04-01 14:03:39 +0800

    n4

--- helloworld.c
+++ helloworld.c
@@ -7,6 +7,14 @@ int main(void)
 	printf("I am a changed line!\n");
 	printf("I am a new line?\n");
 	printf("dakjdhsakdhakhdkahska\n");
+	printf("dakjdhsakdhakhdkahska\n");
+	printf("dakjdhsakdhakhdkahska\n");
+	printf("dakjdhsakdhakhdkahska\n");
+	printf("dakjdhsakdhakhdkahska\n");
+	printf("I am a dorodoro line.\n");
+	printf("I am a dorodoro line.\n");
+	printf("I am a dorodoro line.\n");
+	printf("I am a dorodoro line.\n");
 	printf("I am a dorodoro line.\n");
 
 	return 0;
