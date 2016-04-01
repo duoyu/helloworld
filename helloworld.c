d52dbf08635a9891c2265dedcc1dc2469b74e558
duoyu <275866494@qq.com>
2016-04-01 10:42:08 +0800

    n2

--- helloworld.c
+++ helloworld.c
@@ -6,8 +6,6 @@ int main(void)
 	printf("Helloworld!\n");
 	printf("I am a changed line!\n");
 	printf("I am a new line?\n");
-	printf("I am a new line?\n");
-	printf("I am a new line?\n");
 	printf("dakjdhsakdhakhdkahska\n");
 	printf("I am a dorodoro line.\n");
 
